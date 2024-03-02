52c17761610e67963597509a4452fd396979fd8b
sujoy <sujoydhar95@gmail.com>
2024-03-02 12:18:49 +0600

    scratchpad: watchtower added

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.use(cors({}));
 const port = process.env.port || 3000;
 
 app.get("/api/v1", (req, res) => {
-  res.send("<h2>Hi There,Boss MAN. App is Running</h2>");
+  res.send("<h2>Hi There,Boss MAN/Woman, I am running and this is v2</h2>");
 });
 
 app.use("/api/v1/posts", postRouter);
